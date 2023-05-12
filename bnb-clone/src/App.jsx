75f65b0e6f74a2f12797c82aaf56d99fec859bba
SrParker98 <116755291+SrParker98@users.noreply.github.com>
2023-05-12 13:49:55 +0200

    segundo committed

--- bnb-clone/src/App.jsx
+++ bnb-clone/src/App.jsx
@@ -1,31 +1,17 @@
-
-import './App.css'
-import Header from './Components/Header'
-import Filter from './Components/Filter-bnb/Filter'
-import  Card  from './Components/cards/Card'
-import Filterbar from './Components/Filter-bnb/Filterbar'
-
-
-
-
-
-
-
+import "./App.css";
+import Header from "./Components/Header";
+import Filter from "./Components/Filter-bnb/Filter";
+import Card from "./Components/cards/Card";
+import Filterbar from "./Components/Filter-bnb/Filterbar";
+import Carta from "./Components/cards/Carta";
 function App() {
-  
-
   return (
-    
-<div>
-    <Header/>
-       <Filterbar/>
-       <Card/>
-       
-       
-    
+    <div>
+      <Header />
+      <Filterbar />
+      <Card />
     </div>
-  
-  )
+  );
 }
 
-export default App
+export default App;
--- bnb-clone/src/Components/cards/Card.jsx
+++ bnb-clone/src/Components/cards/Card.jsx
@@ -1,48 +1,46 @@
-import React from 'react'
-import { list2 } from './imagenes.js'
-import './style.css'
-import {FaStar } from 'react-icons/fa'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import React from "react";
+import { list2 } from "./imagenes.js";
+import "./style.css";
+import { FaStar } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default function Card({Card}) {
-const data = list2
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-
-
-
+export default function Card() {
+  const data = list2;
 
   return (
-    
-   
-      
-    <ul className='card-container'>
-      {data.map((data) => (
+    <ul className="card-container">
+      {data.map((data, i) => (
+        <li className="card-img" key={i}>
+          <Swiper
+            slidesPerView={1}
+            navigation={true}
+            pagination={true}
+            mousewheel={true}
+          >
+            <SwiperSlide key={i}>
+              <img src={`${data.imgSrc}`} key={i} className="card-img" />
+            </SwiperSlide>
+          </Swiper>
+          <div className="card-title-rating">
+            <span className="b">{data.title}</span>
+            <span className=" rating">
+              {" "}
+              <FaStar />
+              {data.rating}
+            </span>
+          </div>
 
-
-        <li className='card-img'>
-          
-           <img  className='img' src= {data.imgSrc[0] }  alt="images" />
-           
-            <div className='card-title-rating'>
-
-                  <span className='b'>{data.title}</span>
-                  <span className=' rating'> <FaStar />{data.rating}</span>
-                    
-              </div>
-
-             <div className='price'>
-
-                <span className='opacity-70'>{data.desc}</span>
-                <span className='b '>{data.price}$ <small className='weight'>Night</small> </span>
-
-            </div>
+          <div className="price">
+            <span className="opacity-70">{data.desc}</span>
+            <span className="b ">
+              {data.price}$ <small className="weight">Night</small>{" "}
+            </span>
+          </div>
         </li>
-        
       ))}
     </ul>
-   
-  )
+  );
 }
-
-
