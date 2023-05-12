f6a1c113383c4e7b28d22f737aafcf3bd7b4755e
SrParker98 <116755291+SrParker98@users.noreply.github.com>
2023-05-12 15:29:51 +0200

    3 commit

--- bnb-clone/src/Components/cards/Card.jsx
+++ bnb-clone/src/Components/cards/Card.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { list2 } from "./imagenes.js";
 import "./style.css";
 import { FaStar } from "react-icons/fa";
+import { Pagination, Navigation } from "swiper";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css/navigation";
@@ -9,34 +11,38 @@ import "swiper/css/pagination";
 
 export default function Card() {
   const data = list2;
+  console.log(data);
 
   return (
     <ul className="card-container">
-      {data.map((data, i) => (
-        <li className="card-img" key={i}>
+      {data.map((cardInfo, i) => (
+        <li className="card-img" key={i} style={{ maxWidth: "250px" }}>
           <Swiper
+            modules={[Pagination, Navigation]}
             slidesPerView={1}
             navigation={true}
             pagination={true}
             mousewheel={true}
           >
-            <SwiperSlide key={i}>
-              <img src={`${data.imgSrc}`} key={i} className="card-img" />
-            </SwiperSlide>
+            {cardInfo.imgSrc.map((imageUrl) => (
+              <SwiperSlide key={imageUrl}>
+                <img src={imageUrl} className="card-img" />
+              </SwiperSlide>
+            ))}
           </Swiper>
           <div className="card-title-rating">
-            <span className="b">{data.title}</span>
+            <span className="b">{cardInfo.title}</span>
             <span className=" rating">
               {" "}
               <FaStar />
-              {data.rating}
+              {cardInfo.rating}
             </span>
           </div>
 
           <div className="price">
-            <span className="opacity-70">{data.desc}</span>
+            <span className="opacity-70">{cardInfo.desc}</span>
             <span className="b ">
-              {data.price}$ <small className="weight">Night</small>{" "}
+              {cardInfo.price}$ <small className="weight">Night</small>{" "}
             </span>
           </div>
         </li>
